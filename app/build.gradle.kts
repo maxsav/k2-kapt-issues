1555537c150adfab2ab6d18d3cfdcc76773668d8
Maksim Savoskin <maximsavoskin@gmail.com>
2023-11-10 10:52:50 +0800

    https://youtrack.jetbrains.com/issue/KT-63349

---
+++ app/build.gradle.kts
@@ -0,0 +1,46 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.jetbrainsKotlinKapt)
+}
+
+android {
+    namespace = "com.example.myapplication"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.myapplication"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    compileOptions {
+        dataBinding {
+            enable = true
+        }
+    }
+}
+
+dependencies {
+}
---
+++ app/src/main/java/com/example/myapplication/MyAnnotation.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication
+
+@RequiresOptIn(
+    level = RequiresOptIn.Level.WARNING,
+    message = ""
+)
+@Retention(AnnotationRetention.BINARY)
+annotation class MyAnnotation
